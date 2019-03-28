a943008bc5fb3bf1328f48e8210ba356c7593813
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-03-28 18:06:36 +0700

    a

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,24 +1,89 @@
-﻿var mainApp, mainRouter,
-    mainData = {
+﻿var _MAIN_VUE, _MAIN_ROUTER,
+    _MAIN_DATA = {
         userInfo: {
             loggedIn: false
         }
     };
+
 /**********************************************************************************/
-// mainRouter = new VueRouter({ routerItems });
-mainRouter = new VueRouter();
-mainRouter.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth) && !mainData.userInfo.loggedIn) {
-        next({ path: '/login', query: { redirect: to.fullPath } });
-    } else {
-        next();
+
+function f_viewLoad(viewName, callback) {
+    console.log('VIEW_LOAD ...');
+
+    var file = 'view/' + viewName + '/css.css';
+    var head = document.getElementsByTagName("head")[0];
+
+    var notExist = document.querySelectorAll('#view_js_' + viewName + ', #view_css_' + viewName).length == 0;
+    if (notExist == false) {
+        console.error('VIEW [' + viewName + '] RESOURCE EXIST ...');
+        return;
     }
-});
-mainApp = new Vue({ el: '#app', mainRouter });
-mainRouter.onReady(function () {
-    console.log('ROUTER: done ...');
-});
+
+    var link = document.createElement('link');
+    link.setAttribute('id', 'view_css_' + viewName);
+    link.setAttribute('rel', 'stylesheet');
+    link.href = file;
+    head.appendChild(link);
+
+    file = 'view/' + viewName + '/js.js';
+    var script = document.createElement('script');
+    script.setAttribute('id', 'view_js_' + viewName);
+    script.src = file;
+    script.type = 'text/javascript';
+
+    //real browsers
+    script.onload = function () {
+        //_MAIN_ROUTER.addRoutes([{ path: '/' + viewName.toLocaleLowerCase(), component: window['COM_'.toLocaleUpperCase()] }]);
+        setTimeout(function () {
+            callback();
+        }, 30);
+    };
+
+    ////////Internet explorer
+    //////script.onreadystatechange = function () {
+    //////    if (this.readyState == 'complete') {
+    //////        callback();
+    //////    }
+    //////};
+    head.appendChild(script);
+}
+
 /**********************************************************************************/
+
+function f_mainInit() {
+    console.log('MAIN_INIT ...');
+
+    var router = new VueRouter();
+    router.beforeEach((to, from, next) => {
+        if (to.matched.some(record => record.meta.requiresAuth) && !_MAIN_DATA.userInfo.loggedIn) {
+            next({ path: '/login', query: { redirect: to.fullPath } });
+        } else {
+            next();
+        }
+    });
+    router.onReady(function () {
+        console.log('_MAIN_ROUTER: ready ...');
+    });
+    var app = new Vue({ el: '#app', router });
+
+    _MAIN_ROUTER = router;
+    _MAIN_VUE = app;
+    
+    //Load component login
+    f_viewLoad('login', function () {
+        console.log('LOAD_VIEW: login -> done');
+        f_mainSetup();
+    });
+}
+
+function f_mainSetup() {
+    console.log('MAIN_SETUP ...');
+
+}
+/**********************************************************************************/
+$(document).ready(function () {
+    f_mainInit();
+});
 
 //var About = { template: '<h1>About</div>' };
 //var Dashboard = { template: '<h1>Dashboard</h1>' };
--- view/login/js.js
+++ view/login/js.js
@@ -1,1 +1,10 @@
 ﻿
+var COM_LOGIN = {
+    template: '<input type="submit" value="Login" v-on:click="login">',
+    methods: {
+        login: function () {
+            _MAIN_DATA.userInfo.loggedIn = true;
+            router.push(this.$route.query.redirect);
+        }
+    }
+};
