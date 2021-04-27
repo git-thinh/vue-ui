775a15413384943e88024d5310dfa08afd7a0324
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2021-04-28 02:25:37 +0700

    ret

--- view/log/log-test-ws/js.js
+++ view/log/log-test-ws/js.js
@@ -7,13 +7,25 @@ var LOG_TEST_WS_COM = Vue.component('log-test-ws', {
     mixins: [_MIXIN, _COMS],
     data: function () {
         var data = {
-            _name: 'log-test-ws'
+            _name: 'log-test-ws',
+            socket: {
+
+            }
         };
         return data;
     },
     template: _apiGet('view/log/log-test-ws/index.html'),
     mounted: function () {
+        var _self = this;
+        _self._socketInit();
+
         document.title = 'LOG-TEST';
         $(".alert").alert('close');
+    },
+    methods: {
+        _onOtherViewLoadCompleted: function (viewSetting) { },
+        _socketInit: function (viewSetting) {
+
+        }
     }
 });
